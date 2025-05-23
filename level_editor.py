8624243fa7de267a7cd0f47d5c6c4c6d465f2440
Mutuking <kawaguchi.2525.haruki@gmail.com>
2025-05-23 21:36:32 +0900

    01_08完了

--- level_editor.py
+++ level_editor.py
@@ -17,6 +17,44 @@ bl_info = {
     "category": "Object"
 }
 
+# パネル設定
+class OBJECT_PT_file_name(bpy.types.Panel):
+    """オブジェクトのファイルネームパネル"""
+    bl_idname = "OBJECT_PT_file_name"
+    bl_label = "FileName"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+
+    #サブメニューの描画
+    def draw(self, context):
+        # パネルに項目を追加
+        if "file_name" in context.object:
+            # 既にプロパティがあれば、プロパティを表示
+            self.layout.prop(context.object, '["file_name"]', text = self.bl_label)
+        else:
+            # プロパティがなければ、プロパティ追加ボタンを表示
+            self.layout.operator(MYADDON_OT_add_filename.bl_idname)
+
+        """
+        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text = MYADDON_OT_stretch_vertex.bl_label)
+        self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text = MYADDON_OT_create_ico_sphere.bl_label)
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname, text = MYADDON_OT_export_scene.bl_label)
+        """
+
+# ファイルネームを追加する
+class MYADDON_OT_add_filename(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_add_filename"
+    bl_label = "FileName 追加"
+    bl_description = "['file_name']カスタムプロパティを追加します"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        # カスタムプロパティを追加
+        context.object["file_name"] = ""
+
+        return {"FINISHED"}
+
 
 # オペレータ シーン出力
 class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
@@ -61,7 +99,7 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
             indent += "/t"
 
         # オブジェクト名書き込み
-        self.write_and_print(file, indent + object.type + " - " + object.name)
+        self.write_and_print(file, indent + object.type)
 
         trans, rot, scale = object.matrix_local.decompose()
         rot = rot.to_euler()
@@ -72,6 +110,11 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
         self.write_and_print(file, indent + "Trans(%f, %f, %f)" % (trans.x, trans.y, trans.z))
         self.write_and_print(file, indent + "Rot(%f, %f, %f)" % (rot.x, rot.y, rot.z))
         self.write_and_print(file, indent + "Scale(%f, %f, %f)" % (scale.x, scale.y, scale.z))
+        # カスタムプロパティ 'file_name'
+        if "file_name" in object:
+            self.write_and_print(file, indent + "N %s" % object["file_name"])
+        self.write_and_print(file, indent + 'END')
+
         self.write_and_print(file, '')
 
         # 子ノードへ進む
@@ -183,6 +226,8 @@ classes = (
     MYADDON_OT_stretch_vertex,
     MYADDON_OT_export_scene,
     TOPBAR_MT_my_menu,
+    MYADDON_OT_add_filename,
+    OBJECT_PT_file_name
 )
 
 # テスト実行用コード
