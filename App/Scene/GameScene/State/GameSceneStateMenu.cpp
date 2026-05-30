6f88c73c7b832a89717bd0cecc5bc2382885ac1a
Mutuking <kawaguchi.2525.haruki@gmail.com>
2026-05-30 21:11:52 +0900

    競合の解決

---
+++ App/Scene/GameScene/State/GameSceneStateMenu.cpp
@@ -0,0 +1,41 @@
+#include "GameSceneStateMenu.h"
+#include "scene/GameScene/GameScene.h"
+#include "input/Input.h"
+
+void GameSceneStateMenu::Enter(GameScene& scene)
+{
+	menuState_ = MenuState::Enter;
+	scene.GetInputContext()->SetCanPlayerMove(false);
+	scene.GetInputContext()->SetCanLockOn(false);
+	scene.GetInputContext()->SetCanCameraMove(false);
+}
+
+void GameSceneStateMenu::Update(GameScene& scene)
+{
+	scene.SetSceneTime(0.0f);
+
+	float muskAlpha = scene.GetMuskAlpha();
+
+	switch (menuState_) {
+	case MenuState::Enter:
+		muskAlpha += DeltaTime::GetDeltaTime() * 1.5f;
+
+		if (muskAlpha >= 0.8f) {
+			muskAlpha = 0.8f;
+			scene.GetMenuUI()->Enter();
+			menuState_ = MenuState::Normal;
+		}
+		break;
+	case MenuState::Normal:
+		if (Input::GetInstance()->TriggerKey(DIK_M) || Input::GetInstance()->TriggerButton(ButtonStart)) {
+			scene.ChangeState("Play");
+		}
+		break;
+	}
+	scene.SetMuskAlpha(muskAlpha);
+}
+
+void GameSceneStateMenu::Exit(GameScene& scene)
+{
+	scene.GetMenuUI()->Exit();
+}
